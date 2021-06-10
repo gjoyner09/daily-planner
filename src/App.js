d7ff9ff86bd37d4bf9dedd7a7b6a0f49bfafc4f2
gjoyner09 <gjoyner09@gmail.com>
2021-06-10 15:23:17 +1000

    added basic to do functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css';
 import Calendar from './Components/Calendar'
+import Todo from './Components/To-Do'
 
 function App() {
   return (
     <div className="App">
-      <Calendar></Calendar>
+      <Todo></Todo>
     </div>
   );
 }
