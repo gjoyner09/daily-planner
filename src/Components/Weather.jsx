9622ac6115827732f89ca536bca72f1bff3320ea
gjoyner09 <gjoyner09@gmail.com>
2021-06-10 17:44:33 +1000

    added weather functionality

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,17 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Forecast from './Forecast'
 
-let lat
-let long
-
-navigator.geolocation.getCurrentPosition((position) => {
-lat = position.coords.latitude
-long = position.coords.longitude}, () => {console.log('there is an error')}, {})
-
-
-
-
-
+let city, country
 
 let styles = {
     display:'flex',
@@ -23,39 +13,70 @@ function Weather() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [items, setItems] = useState([]);
+    const [lat, setLat] = useState(null)
+    const [long, setLong] = useState(null)
+
+    let locationPromise = new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            setPosition(position.coords)
+            resolve()
+        })
+    })
+    
+    const setPosition = async (data) => {
+        await setLat(data.latitude)
+        await setLong(data.longitude)
+    }
+    
+    const setLocation = (data) => {
+        city = data.city.toLowerCase()
+        city = city[0].toUpperCase() + city.substring(1)
+        country = data.country
+    }
     
   
     useEffect(() => {
-      fetch("http://www.7timer.info/bin/api.pl?lon=" + long + "&lat=" + lat + "&product=civillight&output=json")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            setIsLoaded(true);
-            setItems(result.dataseries);
-          },
-          (error) => {
-            setIsLoaded(true);
-            setError(error);
-          }
-        )
-  
-  
-        
+        if(true) {
+            locationPromise
+            .then(() => fetch("http://www.7timer.info/bin/api.pl?lon=" + long + "&lat=" + lat + "&product=civillight&output=json"))
+            .then(res => res.json())
+            .then(
+            (result) => {
+                setIsLoaded(true);
+                setItems(result.dataseries);
+            },
+            (error) => {
+                setIsLoaded(true);
+                setError(error);
+            }
+            )
+            .then(() => console.log("then: " + lat))
+            .then(() => console.log("then: " + long))
+            // .then(() => fetch(`https://geocode.xyz/${lat},${long}?geoit=json`))
+            // .then(res => res.json())
+            // .then(data => setLocation(data))
+        }
     }, [])
-  
-
- 
-  
+    
     return (
       <div className="Weather" style={styles}>
-
-          {items.map(function(item, index){
-              return (
-                  <Forecast key={index} index={index} cloudcover={item.weather} temperatureMax={item.temp2m.max} temperatureMin={item.temp2m.min}/>
-              )
-          })}
-         
-        
+          {!isLoaded &&
+            <>
+             <div class="loader"></div>
+             <p>Loading weather</p>
+            </>
+          }
+          {isLoaded && 
+                <>
+                    <h1>Weather for {city}, {country}</h1>
+                    {lat && items.map(function(item, index){
+                        return (
+                            <Forecast key={index} index={index} cloudcover={item.weather} temperatureMax={item.temp2m.max} temperatureMin={item.temp2m.min} />
+                        )
+                        })
+                    }
+                </>
+            }
       </div>
     );
 }
