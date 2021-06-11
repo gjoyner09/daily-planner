bdd9e87f5ac1521e44f1f80f7f9b8bc3ab2032f9
gjoyner09 <gjoyner09@gmail.com>
2021-06-11 12:03:46 +1000

    added weather functionality for other cities and loading icon

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Forecast from './Forecast'
 
-let city, country
-
 let styles = {
     display:'flex',
     flexDirection: 'row',
@@ -13,63 +11,71 @@ function Weather() {
     const [error, setError] = useState(null);
     const [isLoaded, setIsLoaded] = useState(false);
     const [items, setItems] = useState([]);
-    const [lat, setLat] = useState(null)
-    const [long, setLong] = useState(null)
+    const [position, setPosition] = useState(null)
+    const [location, setLocation] = useState(null)
+    const [searchText, setSearchText] = useState('')
+    const [searchLocation, setSearchLocation] = useState('')
 
-    let locationPromise = new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            setPosition(position.coords)
-            resolve()
-        })
-    })
-    
-    const setPosition = async (data) => {
-        await setLat(data.latitude)
-        await setLong(data.longitude)
-    }
-    
-    const setLocation = (data) => {
-        city = data.city.toLowerCase()
-        city = city[0].toUpperCase() + city.substring(1)
-        country = data.country
-    }
-    
-  
     useEffect(() => {
-        if(true) {
-            locationPromise
-            .then(() => fetch("http://www.7timer.info/bin/api.pl?lon=" + long + "&lat=" + lat + "&product=civillight&output=json"))
-            .then(res => res.json())
-            .then(
+        navigator.geolocation.getCurrentPosition((data) => {
+            setPosition({latitude: data.coords.latitude, longitude: data.coords.longitude})
+        })
+    }, [])
+
+    
+    useEffect(() => {
+        position && fetch("http://www.7timer.info/bin/api.pl?lon=" + position.longitude + "&lat=" + position.latitude + "&product=civillight&output=json")
+        .then(res => res.json())
+        .then(
             (result) => {
                 setIsLoaded(true);
                 setItems(result.dataseries);
-            },
-            (error) => {
-                setIsLoaded(true);
-                setError(error);
             }
-            )
-            .then(() => console.log("then: " + lat))
-            .then(() => console.log("then: " + long))
-            // .then(() => fetch(`https://geocode.xyz/${lat},${long}?geoit=json`))
-            // .then(res => res.json())
-            // .then(data => setLocation(data))
-        }
-    }, [])
+        )
+        .catch((error) => console.log(error))
+        
+        position && fetch(`https://geocode.xyz/${position.latitude},${position.longitude}?geoit=json`)
+        .then(res => res.json())
+        .then(data => {
+            let city = data.city
+            city = city[0] + city.substring(1).toLowerCase()
+            city = city.split('').map((letter, index) => city[index - 1] === " " ? letter.toUpperCase() : letter).join('')
+            setLocation({city: city, country: data.country})
+        })
+        .catch((error) => console.log(error))
+    }, [position])
+    
+    const handleChange = (event) => {
+        setSearchText(event.target.value)
+    }
+    
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setIsLoaded(false)
+        fetch(`https://geocode.xyz/${searchText.replaceAll(' ', '%20')}?json=1`)
+        .then(res => res.json())
+        .then(data => {
+            setPosition({latitude: Number(data.latt), longitude: Number(data.longt)})
+        })
+    }
     
     return (
       <div className="Weather" style={styles}>
+          <form onSubmit={handleSubmit}>
+                <label htmlFor="weatherLocation">Search city:</label>
+                <input type="text" id="weatherLocation" name="weatherLocation" onChange={handleChange}/>
+                <input type="submit" />
+            </form>
           {!isLoaded &&
             <>
-             <div class="loader"></div>
+             <div className="loader"></div>
              <p>Loading weather</p>
             </>
           }
           {isLoaded && 
                 <>
-                    <h1>Weather for {city}, {country}</h1>
-                    {lat && items.map(function(item, index){
+                    {location && location.city && <h1>Weather for {location.city}, {location.country}</h1>}    
+                    {items.map(function(item, index){
                         return (
                             <Forecast key={index} index={index} cloudcover={item.weather} temperatureMax={item.temp2m.max} temperatureMin={item.temp2m.min} />
                         )
